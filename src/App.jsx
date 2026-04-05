21365ccd651218e76b655b155f0b553a8282d9d6
7atim <fadoulhatim@gmail.com>
2026-04-05 21:20:30 +0100

    Fix case sensitivity: Portfolio import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Portfolio from './pages/portfolio';
+import Portfolio from './pages/Portfolio';
 import ProjectModal from './components/ProjectModal';
 import { personalInfo } from './data/projects';
 import { LanguageProvider, useLanguage } from './context/LanguageContext';
